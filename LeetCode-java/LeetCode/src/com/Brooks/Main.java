4cf6576ecf2e789a29a46b1eba28176a9aab3901
BrooksWon <jianyu996@163.com>
2019-08-12 19:39:14 +0800

    java刷一下leetcode

---
+++ LeetCode-java/LeetCode/src/com/Brooks/Main.java
@@ -0,0 +1,14 @@
+package com.Brooks;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int[] nums = {2, 7, 11, 15};
+
+        for (int i:Two_Num.twoNum(nums,9)
+             ) {
+            System.out.println(i);
+        }
+    }
+}
